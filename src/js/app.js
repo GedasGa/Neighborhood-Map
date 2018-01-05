3cf12f38fd38257c9039e56c61c316511fc29684
unknown <gedas.gardauskas@gmail.com>
2018-01-05 14:53:19 +0200

    Link knockout script. Add markers and map styles model. Change Google Maps API callback function. Create a new Map with custom styles.

---
+++ src/js/app.js
@@ -0,0 +1,146 @@
+// Data model
+// ----------------------------------------------------------------- //
+var markersModel = function() {
+    currentMarker: ko.obervable(null);
+    markers: [
+        { 
+            title: "Quadrum",
+            location: { lat: 54.698355, lng: 25.270992 },
+            address: "Konstitucijos pr. 21" 
+        },
+        { 
+            title: "Swedbank",
+            location: { lat: 54.695926, lng: 25.273732 }, 
+            address: "Konstitucijos pr. 20A" 
+        },
+        { 
+            title: "Europa", 
+            location: { lat: 54.696214, lng: 25.276796 }, 
+            address: "Konstitucijos pr. 7" 
+        },
+        { 
+            title: "K29", 
+            location: { lat: 54.699611, lng: 25.265009 }, 
+            address: "Konstitucijos pr. 29" 
+        },
+        { 
+            title: "3 Burės",
+            location: { lat: 54.696533, lng: 25.279056 }, 
+            address: "Lvovo g. 25" 
+        }
+    ];
+};
+
+var stylesArray = [
+    { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+    { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+    { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+    {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+    },
+    {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#263c3f' }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#6b9a76' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3d19c' }]
+    },
+    {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+    },
+    {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#17263c' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
+    }
+];
+// ----------------------------------------------------------------- //
+
+var ViewModel = function () {
+    var self = this;
+
+    // Create a map variable
+    var map;
+    
+    // Create a new blank array for all the listing markers.
+    this.markers = ko.observableArray([]);
+
+    // Function to initialize the map within the map div
+    this.initMap = function () {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: new google.maps.LatLng(54.698052, 25.271241),
+            zoom: 15,
+            styles: stylesArray,
+            mapTypeControl: false
+        };
+        // Constructor creates a new Map inside of the given HTML container.
+        map = new google.maps.Map(mapCanvas, mapOptions);
+
+    };
+
+    this.initMap();
+
+};
+
+// Google Maps API callback function for starting the appl
+function startApp() {
+    ko.applyBindings(new ViewModel());
+};
